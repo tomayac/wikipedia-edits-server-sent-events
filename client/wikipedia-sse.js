fe2ecdb226d439a7dd1aa7caccd8862c68ab7d36
Thomas Steiner <steiner.thomas@gmail.com>
2013-10-14 13:42:53 +0200

    Polished the client app.

--- client/wikipedia-sse.js
+++ client/wikipedia-sse.js
@@ -3,29 +3,67 @@
   var ranges = [];
   var botsVsWikipedians = {};
 
+  var contentDiv = document.querySelector('#content');
+  var considerWikidata = document.querySelector('#consider-wikidata');
+
+  considerWikidata.addEventListener('change', function() {
+    if (considerWikidata.checked) {
+      document.querySelector('.wikidata').style.display = 'block';
+    } else {
+      document.querySelector('.wikidata').style.display = 'none';
+    }
+  });
+
   var createStatsPane = function(language, bots, wikipedians) {
     var div = document.createElement('div');
+    if (language === 'global') {
+      div.style.marginBottom = '2em';
+    }
+    if (language === 'wikidata') {
+      div.setAttribute('class', 'wikidata');
+    }
     var botsImg = document.createElement('img');
-    botsImg.src = 'machine.png';
+    botsImg.src = 'bots.png';
+    botsImg.setAttribute('class', 'icon');
     div.appendChild(botsImg);
     var range = document.createElement('input');
     range.type = 'range';
     div.appendChild(range);
     range.id = language + '-range';
     var wikipediansImg = document.createElement('img');
-    wikipediansImg.src = 'man.png';
+    wikipediansImg.src = 'wikipedians.png';
+    wikipediansImg.setAttribute('class', 'icon');
     div.appendChild(wikipediansImg);
-    var lang = document.createElement('span');
-    lang.innerHTML = ' ' + language;
+    var logo = document.createElement('img');
+    logo.src = language === 'wikidata' ? 'wikidata.png' : 'wikipedia.png';
+    logo.setAttribute('class', 'icon logo');
+    div.appendChild(logo);
+    var lang;
+    if (language !== 'global') {
+      lang = document.createElement('a');
+      lang.href = language === 'wikidata' ?
+          'http://' + language + '.org/' :
+          'http://' + language + '.wikipedia.org/';
+      lang.textContent = language;
+      lang.setAttribute('target', '_blank');
+    } else {
+      lang = document.createElement('span');
+      lang.textContent = language;
+    }
     div.appendChild(lang);
     var label = document.createElement('label');
     label.setAttribute('for', '#' + language + '-range');
     label.id = language + '-label';
-    label.innerHTML = ' (' + bots + ' vs. ' + wikipedians + ')';
+    label.innerHTML = updateStatsHtml(bots, wikipedians);
     div.appendChild(label);
     var fragment = document.createDocumentFragment();
     fragment.appendChild(div);
     return fragment;
+  };
+
+  var updateStatsHtml = function(bots, wikipedians) {
+    return ' <small>(' + bots + ' <i>vs.</i> ' + wikipedians +
+        ')</small>';
   };
 
   setInterval(function() {
@@ -37,7 +75,7 @@
       range.max = bots + wikipedians;
       range.value = range.max - bots;
       var label = document.querySelector('#' + language + '-label');
-      label.innerHTML = ' (' + bots + ' vs. ' + wikipedians + ')';
+      label.innerHTML = updateStatsHtml(bots, wikipedians);
     };
 
     ranges.forEach(function(language) {
@@ -46,10 +84,14 @@
       globalBots += stats.bots;
       globalWikipedians += stats.wikipedians;
     });
+    if (!considerWikidata.checked) {
+      globalBots -= botsVsWikipedians['wikidata'].bots;
+      globalWikipedians -= botsVsWikipedians['wikidata'].wikipedians;
+    }
     updateStats('global', globalBots, globalWikipedians);
-  }, 1000);
+  }, 500);
 
-  document.body.appendChild(createStatsPane('global', 0, 0));
+  contentDiv.appendChild(createStatsPane('global', 0, 0));
 
   var source = new EventSource('http://localhost:8080');
   source.addEventListener('message', function(e) {
@@ -61,7 +103,7 @@
         wikipedians: data.isBot ? 0 : 1
       }
       var stats = botsVsWikipedians[data.language];
-      document.body.appendChild(createStatsPane(
+      contentDiv.appendChild(createStatsPane(
           data.language, stats.bots, stats.wikipedians));
     }
     if (data.isBot) {
